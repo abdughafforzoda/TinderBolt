8d2f1bd0cb42b289487aa1aef55e5ad931328ef7
Yusuf Abdughafforzoda <126441562+abdughafforzoda@users.noreply.github.com>
2024-07-31 22:44:40 +0500

    Update TinderBoltApp.java

--- src/main/java/com/example/telegram/TinderBoltApp.java
+++ src/main/java/com/example/telegram/TinderBoltApp.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 
 public class TinderBoltApp extends MultiSessionTelegramBot {
     public static final String TELEGRAM_BOT_NAME = "tinder_with_ai_bot"; //TODO: добавь имя бота в кавычках
-    public static final String TELEGRAM_BOT_TOKEN = "7460003170:AAFTrHDjYGs0NwabO8dUupPn54AXM2Se5pA"; //TODO: добавь токен бота в кавычках
+    public static final String TELEGRAM_BOT_TOKEN = ""; //TODO: добавь токен бота в кавычках
 
     private ChatGPTService chatGPT = new ChatGPTService(OPEN_AI_TOKEN);
     private DialogMode currentMode = null;
